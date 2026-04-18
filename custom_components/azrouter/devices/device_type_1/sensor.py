f7ae7ffc7ac468aea0021099fa48f6fbaef78379
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-04-18 15:52:27 +0200

    Update sensor.py
    Corrected import of sensor base class

--- custom_components/azrouter/devices/device_type_1/sensor.py
+++ custom_components/azrouter/devices/device_type_1/sensor.py
@@ -13,7 +13,11 @@ from __future__ import annotations
 from typing import Any, Dict, List, Tuple
 import logging
 
-from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorDeviceClass,
+    SensorStateClass, #NEW
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfSoundPressure, UnitOfPower, UnitOfTemperature
 from homeassistant.helpers.entity import EntityCategory
