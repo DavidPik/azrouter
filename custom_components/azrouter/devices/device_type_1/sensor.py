04360b1369dc3c9b7344e2af75dc0b6ef70fb996
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-04-19 08:46:05 +0200

    Update sensor.py
    Added state_class do construktoru numeric senzoru

--- custom_components/azrouter/devices/device_type_1/sensor.py
+++ custom_components/azrouter/devices/device_type_1/sensor.py
@@ -404,6 +404,7 @@ class DeviceNumericSensor(DeviceBase, SensorEntity):
         devclass: SensorDeviceClass | None = None,
         icon: str | None = None,
         entity_category: EntityCategory | None = None,
+        state_class: SensorStateClass | None = None,
     ) -> None:
         super().__init__(
             coordinator=coordinator,
@@ -417,6 +418,7 @@ class DeviceNumericSensor(DeviceBase, SensorEntity):
             icon=icon,
             entity_category=entity_category,
             model=MODEL_DEVICE_TYPE_1,
+            state_class=state_class,   # NEW
         )
 
     @property
